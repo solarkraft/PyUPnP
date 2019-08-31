46194215ca8ee7996160df25851920c254d2cb71
Paul Hendrix <mail@pahen.de>
2019-08-31 04:56:08 +0200

    comment out and add thoughts to func_params

--- pyupnp/services/__init__.py
+++ pyupnp/services/__init__.py
@@ -161,8 +161,14 @@ class ServiceActionWrapper:
             arguments = self.service.actions[self.name]
 
             # Set arguments 'parameterName' attribute from function spec
-            self.func_params = self.func.func_code.co_varnames
-            for x in xrange(1, len(self.func_params)):
+            # self.func_params = self.func.func_code.co_varnames
+
+            # func only supplies an actionName. we will have to find out its parameters from Service.actions, classifying by in/out. 
+            # example for Browse: ('self', 'objectID', 'browseFlag', 'browseFilter', 'startingIndex', 'requestedCount', 'sortCriteria')
+
+            self.func_params = []
+            
+            for x in range(1, len(self.func_params)):
                 j = x - 1
 
                 param = self.func_params[x]
